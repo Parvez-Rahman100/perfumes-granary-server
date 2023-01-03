04f7b4cfc8538e3c2c8fd07e0271ac53cec60bfa
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2023-01-03 21:45:10 +0600

    deploying in vercel

--- index.js
+++ index.js
@@ -1,100 +1,86 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@perfumesgranary.i6yaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run (){
-    try{
-        await client.connect();
-        const perfumeCollection = client.db('perfumeGranary').collection('products');
-        
-            // Products
-        app.get('/product',async(req,res)=>{
-            const query = {};
-            const cursor = perfumeCollection.find(query);
-            const perfumes = await cursor.toArray();
-            res.send(perfumes);
-        })
+async function run() {
+  try {
+    client.connect();
+    const perfumeCollection = client
+      .db("perfumeGranary")
+      .collection("products");
 
-            //Products Details
-        app.get('/productDetails/:id',async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id : ObjectId(id)};
-            const perfume = await perfumeCollection.findOne(query);
-            res.send(perfume);
-            
-        })
+    // Products
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = perfumeCollection.find(query);
+      const perfumes = await cursor.toArray();
+      res.send(perfumes);
+    });
 
+    //Products Details
+    app.get("/productDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const perfume = await perfumeCollection.findOne(query);
+      res.send(perfume);
+    });
 
-            // ADD
-        app.post('/productDetails',async(req,res)=>{
-            const newProduct = req.body;
-            const result = await perfumeCollection.insertOne(newProduct);
-            res.send(result);
-        })
+    // ADD
+    app.post("/productDetails", async (req, res) => {
+      const newProduct = req.body;
+      const result = await perfumeCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
-            // DELETE
-        app.delete('/productDetails/:id', async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id : ObjectId(id)}
-            const result = await perfumeCollection.deleteOne(query);
-            res.send(result);
-        })
+    // DELETE
+    app.delete("/productDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await perfumeCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-
-
-    app.put('/productDetails/:id', async (req, res) => {
-        const id = req.params.id;
-        console.log(id);
-        const newQuantity = req.body;
-        console.log(quantity);
-        const filter = { _id: ObjectId(id) }
-        const options = { upsert: true };
-        const updatedQuantity = {
-            $set:{
-                quantity: newQuantity.quantity
-            }
-        }
-        const result = await perfumeCollection.updateOne(filter, updatedQuantity, options)
-        res.send(result)
-    })
-
-
-    
-    }
-    finally{
-
-    }
+    app.put("/productDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const newQuantity = req.body;
+      console.log(quantity);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedQuantity = {
+        $set: {
+          quantity: newQuantity.quantity,
+        },
+      };
+      const result = await perfumeCollection.updateOne(
+        filter,
+        updatedQuantity,
+        options
+      );
+      res.send(result);
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("running");
+});
 
-
-
-
-app.get('/', (req,res)=>{
-    res.send('running')
-})
-
-
-
-app.listen(port,()=>{
-    console.log('listenning to port', port);
-})
-
-
-
+app.listen(port, () => {
+  console.log("listenning to port", port);
+});
